bda7d9a3fb6bc1786e33432ad80a5ca82c01daa7
Theo8436 <yssaanascimento@gmail.com>
2026-07-28 17:40:21 -0300

    por hj e so bbs

--- routes/web.php
+++ routes/web.php
@@ -15,43 +15,13 @@ use App\Http\Middleware\LogAcessoMiddleware;
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
 
-Route::prefix('/loja')->group(function(){
-    Route::get('/home', [App\Http\Controllers\LojaController::class, 'home'])->name('loja.home');
-}); 
+Route::get('/professor', [App\Http\Controllers\Professor::class, 'professor']);
+Route::get('/login', [App\Http\Controllers\Login::class, 'login']);
+Route::get('/calendario', [App\Http\Controllers\Calendario::class, 'calendario']);
+Route::get('/revisao', [App\Http\Controllers\Revisao::class, 'revisao']);
 
-Route::prefix('/discos')->group(function(){
-    Route::get('/disco', [App\Http\Controllers\DiscoController::class, 'disco'])->name('disco.disco');
 
-}); 
 
-Route::prefix('/about')->group(function(){
-    Route::get('/sobre', [App\Http\Controllers\SobreController::class, 'sobre'])->name('about.sobre');
 
-}); 
-
-Route::prefix('/buy')->group(function(){
-    Route::get('/comprar', [App\Http\Controllers\ComprarController::class, 'comprar'])->name('buy.comprar');
-
-}); 
-
-Route::prefix('/lmolm')->group(function(){
-    Route::get('/compra', [App\Http\Controllers\CompraController::class, 'compra'])->name('lmolm.compra');
-
-});
-
-Route::prefix('/btms')->group(function(){
-    Route::get('/compras', [App\Http\Controllers\ComprasController::class, 'compras'])->name('btms.compras');
-
-});
-
-Route::prefix('/ydkwli')->group(function(){
-    Route::get('/quero', [App\Http\Controllers\QueroController::class, 'quero'])->name('ydkwli.quero');
-
-});
-
-Route::prefix('/see')->group(function(){
-    Route::get('/ver', [App\Http\Controllers\VerController::class, 'ver'])->name('see.ver');
-
-});
 
 
